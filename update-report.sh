3719e84fe1deffac9a5890044655067c9a976563
Shen Jiamin <shen_jiamin@comp.nus.edu.sg>
2024-06-24 14:36:10 +0800

    refactor coursereg report fetcher
    Signed-off-by: Shen Jiamin <shen_jiamin@comp.nus.edu.sg>

--- update-report.sh
+++ update-report.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+DIR=repo
+
+export TZ=Asia/Singapore
+
+set -ex
+
+cat <<EOF | wget --no-verbose -i -
 https://www.nus.edu.sg/ModReg/docs/DemandAllocationRptGD_R0.pdf
 https://www.nus.edu.sg/ModReg/docs/DemandAllocationRptGD_R1.pdf
 https://www.nus.edu.sg/ModReg/docs/DemandAllocationRptGD_R2.pdf
@@ -11,3 +20,17 @@ https://www.nus.edu.sg/ModReg/docs/VacancyRpt_R0.pdf
 https://www.nus.edu.sg/ModReg/docs/VacancyRpt_R1.pdf
 https://www.nus.edu.sg/ModReg/docs/VacancyRpt_R2.pdf
 https://www.nus.edu.sg/ModReg/docs/VacancyRpt_R3.pdf
+EOF
+
+for file in *.pdf; do
+    DATE=$(date -r $file -Is)
+    if [ -f $DIR/$file ] && cmp -s $file $DIR/$file; then
+        echo "No change in $file"
+    else
+        cp $file $DIR
+        pushd $DIR
+        git add $file
+        git commit -m "Update $file to $DATE" --date="$DATE"
+        popd
+    fi
+done
